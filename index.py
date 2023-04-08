78ebc10e13ee98f084b330a1fea80031802f6901
Miron Barykin <barykinmiron@gmail.com>
2023-04-08 11:49:42 +0200

    initial commit

---
+++ index.py
@@ -0,0 +1,12 @@
+import discord
+import os
+
+class Client(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = Client(intents=intents)
+client.run(os.getenv('DISCORD_BOT_TOKEN'))
