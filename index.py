fd74ab8dedf258e093b8b43d417935cc1c054736
Miron Barykin <barykinmiron@gmail.com>
2023-04-11 11:21:49 +0200

    0.1.2

--- index.py
+++ index.py
@@ -4,28 +4,22 @@ from decouple import config
 import services
 
 TARGET_GUILD = discord.Object(id=config('GUILD_ID'))
-
+CARS = services.database.Connection().get('cars')
+CAR_CHOICES = [discord.app_commands.Choice(name=f'{CARS[car][0]}', value=f'{car}') for car in range(len(CARS))]
 client = services.general.Client()
 
 
-@client.tree.command(name='cars_take', description='Взять машину.', guild=TARGET_GUILD)
-@discord.app_commands.choices(car=[discord.app_commands.Choice(name=f'{car[0]}', value='0') for car in services.database.Connection().get('cars')])
+@client.tree.command(name='car', description='Взять машину.', guild=TARGET_GUILD)
+@discord.app_commands.choices(car=CAR_CHOICES)
 async def command_car(interaction, car: discord.app_commands.Choice[str], comment: str = None):
-    print(car)
-    # TODO; DISCORD IS ALWAYS SENDING ONLY FIRST CAR, NOT WHAT I'AM CHOOSING
     response = services.cars.TakingReport(interaction.user, car.name, comment).response()
     await interaction.response.send_message(**response)
 
 
-@client.tree.command(name='cars_status', description='Статус машин.', guild=TARGET_GUILD)
+@client.tree.command(name='cars', description='Статус машин.', guild=TARGET_GUILD)
 async def command_cars(interaction):
     response = services.cars.StatusReport(interaction.user).response()
     await interaction.response.send_message(**response)
-
-
-@client.tree.command(name='car_add', description='Обновление автопарка.', guild=TARGET_GUILD)
-async def command_cars(interaction, name: str):
-    pass
 
 
 @client.event
